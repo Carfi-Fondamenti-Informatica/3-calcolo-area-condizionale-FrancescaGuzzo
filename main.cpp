627962c29d3b0d1abbae0313a90386ad575fba80
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-03-29 21:28:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main() {
+   float a, b, triangolo, quadrato, rettangolo;
+    int opzione;
+    cout << "inserire due valori reali: " << endl;
+    cin >> a >> b;
+
+    cout << "inserire il valore di opzione: ";
+    cin >> opzione;
+
+    switch (opzione){
+        case 0: 
+            triangolo= (a*b)/2;
+            cout << triangolo << endl;
+            break;
+        case 1: 
+            quadrato= a*a;
+            cout << quadrato << endl;
+            break;
+        case 2: 
+            rettangolo= a*b;
+            cout << rettangolo << endl;
+            break;
+        default:
+            cout << "opzione non valida";
+    }
    return 0;
 }
